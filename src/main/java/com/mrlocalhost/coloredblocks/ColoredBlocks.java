1a0b31122b34a2aacc13f811965dab9331e403bb
Zak <zak.konie@gmail.com>
2023-04-11 19:12:24 -0500

    Initial commit

---
+++ src/main/java/com/mrlocalhost/coloredblocks/ColoredBlocks.java
@@ -0,0 +1,34 @@
+package com.mrlocalhost.coloredblocks;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ColoredBlocks.MOD_ID)
+public class ColoredBlocks {
+    public static final String MOD_ID = "colored_blocks";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ColoredBlocks() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
